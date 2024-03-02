bfc2aa93f6f3cf65bccf372a4cbcdc77dcd0ccab
Aleksandr Volik <Aleksandr_volik@mail.ru>
2024-03-02 13:53:57 +0300

    Initial commit

---
+++ src/test/java/VacationServiceTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import ru.netology.freelancermvn.service.VacationService;
+
+public class VacationServiceTest {
+    @Test
+    void test1() {
+        VacationService service = new VacationService();
+        int result = service.calculateVacationService(10_000, 3_000, 20_000);
+        System.out.println(result);
+    }
+
+    @Test
+    void test2() {
+        VacationService service = new VacationService();
+        int result = service.calculateVacationService(100_000, 60_000, 150_000);
+        System.out.println(result);
+    }
+}
