8c240a10f3ffa4c6d74df3471efce345cf428d1e
Aleksandr Volik <Aleksandr_volik@mail.ru>
2024-03-02 16:53:17 +0300

    Parameterized Test

--- src/test/java/ru/netology/freelancermvn/service/VacationServiceTest.java
+++ src/test/java/ru/netology/freelancermvn/service/VacationServiceTest.java
@@ -1,19 +1,26 @@
 package ru.netology.freelancermvn.service;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 //import ru.netology.freelancermvn.service.VacationService;
 
 public class VacationServiceTest {
-    @Test
-    void test1() {
+    @ParameterizedTest
+    @CsvSource({
+            "10000, 3000, 20000",
+            "100000, 60000, 150000"
+    })
+    public void testVacationService(int income, int expenses, int threshold) {
         VacationService service = new VacationService();
-        int result = service.calculateVacationService(10_000, 3_000, 20_000);
-        System.out.println(result);
+        int result = service.calculateVacationService(income, expenses, threshold);
+        System.out.println(" месяцев отпуска в году " + result);
     }
 
-    @Test
-    void test2() {
-        VacationService service = new VacationService();
-        int result = service.calculateVacationService(100_000, 60_000, 150_000);
-        System.out.println(result);
-    }
+//    @Test
+//    void test2() {
+//        VacationService service = new VacationService();
+//        int result = service.calculateVacationService(100_000, 60_000, 150_000);
+//        System.out.println(result);
+//    }
 }
